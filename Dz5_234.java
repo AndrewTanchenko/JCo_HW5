f8f268a0718a01bc51621cbddd5d623373e2d1ec
AndrewTanchenko <oto.kun.sama@gmail.com>
2017-02-25 12:46:03 +0200

    Add files via upload

---
+++ Dz5_234.java
@@ -0,0 +1,78 @@
+// Javacore / Tanchenko A.
+/*
+ * 2. Дано целое число N (> 0). Используя операции деления нацело и взятия остатка от деления, вывести все его цифры, начиная с самой правой (разряда единиц).
+ * 3. Дано целое число N (> 1). Если оно является простым, то есть не имеет положительных делителей, кроме 1 и самого себя, то вывести True, иначе вывести False.
+ * 4. Дано целое число N (> 1). Последовательность чисел Фибоначчи FK определяется следующим образом: F1 = 1, F2 = 1, FK = FK–2 + FK–1, K = 3, 4, … . 
+ * Проверить, является ли число N числом Фибоначчи. Если является, то вывести True, если нет — вывести False.
+ */
+//import java.util.Scanner;
+//import java.io.*;
+//import java.util.Arrays;
+
+class Dz5_234 {
+  
+  //2)  
+  static  void printAllDigits(int n) {
+      System.out.print (""+n+" is contained next digits:");
+      String s1=""+n;
+      int l=s1.length();
+      for(int i=1;i<=l;i++){
+        System.out.print(" "+n%10);
+        n=(int)(n/10);
+      }
+      System.out.println();
+    }
+    
+  //3)  
+  static  boolean checkPrimeNum(int n) {
+      boolean answer=false;
+      int i=0;
+      for(i=2;i<n;i++){
+        if(n%i==0){break;}
+      }
+      if(i==n){answer=true;}
+      //System.out.println(""+i);
+      return answer;
+    }
+    
+  //4)    
+  static  boolean compareFibonachi(int n) {
+    boolean ans=false;
+    int f_1=1,f_2=1,f_3=0;
+    
+    while(f_3<=n){
+      f_3=f_1+f_2;
+      f_1=f_2;
+      f_2=f_3;
+      //System.out.print(" "+f_3);
+      if(f_3==n){ans=true;}
+    }
+    //System.out.println();
+    return ans;
+  }
+    
+
+
+    public static void main(String[] args) {
+      
+    // Input user data block  
+      int n=0;
+    ////////////////////////
+      
+      n=12345;
+      printAllDigits(n);
+      
+      n=7;
+      if(checkPrimeNum(n)){
+        System.out.println(""+n+" is a prime number ");}
+      else{
+        System.out.println(""+n+" is not a prime number ");}
+      
+      n=21;
+      if(compareFibonachi(n)){
+        System.out.println(""+n+" is a part of Fibonacci series ");}
+      else{
+        System.out.println(""+n+" is not a part of Fibonacci series ");}
+      
+    }
+}
---
+++ Dz5_5.java
@@ -0,0 +1,52 @@
+// Javacore / Tanchenko A.
+/*
+ * Given a positive integer, N, define the ’3N+1’ sequence starting from N as follows: 
+ * If N is an even number, then divide N by two; but if N is odd, then multiply N by 3 and add 1. 
+ * Continue to generate numbers in this way until N becomes equal to 1. 
+ * For example, starting from N = 3, which is odd, we multiply by 3 and add 1, giving N = 3*3+1 = 10. 
+ * Then, since N is even, we divide by 2, giving N = 10/2 = 5. We continue in this way, stopping when we reach 1, 
+ * giving the complete sequence: 3, 10, 5, 16, 8, 4, 2, 1. 
+ * 
+ * Write a program that will read a positive integer from the user and will print out the 3N+1 sequence starting from that integer. 
+ * The program should also count and print out the number of terms in the sequence.
+ */
+//import java.util.Scanner;
+//import java.io.*;
+//import java.util.Arrays;
+
+class Dz5_5 {
+  
+  //5)  
+  static  int even(int n) {
+    int n_=n/2;
+    return n_;}
+  
+  static  int odd(int n) {
+    int n_=n*3+1;
+    return n_;}
+  
+  static  int nextN3plus1(int n) {
+    int n_=0;
+    if(n%2==0){
+      n_=even(n);}
+    else{
+      n_=odd(n);}
+    return n_;}
+    
+ 
+    public static void main(String[] args) {
+      
+    // Input user data block  
+      int n=5;
+    ////////////////////////
+      
+      System.out.println(" start of series: "+n);
+      
+      int i=0;
+      while(n>1){
+        i++;
+        n=nextN3plus1(n);
+        System.out.println(" "+i+" term of series: "+n);
+      }       
+    }
+}
